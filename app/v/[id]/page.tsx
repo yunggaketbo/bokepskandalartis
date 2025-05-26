c30ba9b0eb55141af81c173558ac829d81b70b2a
yunggaketbo <yunggaketbo@gmail.com>
2025-05-26 22:54:10 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -135,22 +135,7 @@ export default async function Video({ params }: PageProps) {
         />
         {/* ... */}
         </section>
-                        <iframe
-                className="w-full h-[30vh] md:h-[55vh] lg:h-[70vh]"
-                src={`https://filemoon.to/e/${file.file_code}`}
-                scrolling="no"
-                title={file.title}
-                frameBorder={0}
-                allowFullScreen={true}
-            ></iframe>
-            <Card className="mx-2 mb-8">
-                <CardHeader>
-                    <CardTitle className="text-xl md:text-3xl font-bold">
-                        {file.file_title}
-                    </CardTitle>
-                </CardHeader>
-            <p>{file.file_title} di {SITENAME} Video Bokep Indo Viral Terbaru Xpanas Bocil Ngentot Jilbab Smp Mama Sma Jepang Jav Barat Simontok hub sotwe olmek avtube pijat pure gudang pemerkosaan rumah tobrut inggris ngintip vcs binor yandex update remaja {SITENAME} wiki raja bokeptube full porno colmeka simontok {file.file_title} playbokep indobokep xpanasonline indoh janda streaming jepang barat korea japan jav cina japanese china rusia arab india thailand hd anime hentai bokepind gudang avtub pijat sotwe rumah pemerkosaan inggris xpanas pure tobrut vcs ngintip binor {SITENAME} remaja yandex update perselingkuhan wiki raja full com porno indoh Hotbabes Big Tits Family Freeporn Ass Naked Celebs Nude Cam Hot Videos Fucking Free Porno Adult Movies Mom Freesex Nudelive Cams Women Stepmom Hotwife Bigtits Melons Tube Huge Tits Pornos Film x Titshits Pornofilme Nice Natural Fuq Girls Teen Sex Pornstars Tube Dick Jihad</p>
-            </Card>
+bajingankau124!
             <h2 className="text-2xl font-bold text-center my-4">Related Video {file.file_title}
             </h2>
             <SearchCardList query={file.file_title.split(" ")[2]} />
