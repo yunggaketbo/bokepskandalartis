0e69aab6e0faed7da13d9f3099d41e5deb21d584
yunggaketbo <yunggaketbo@gmail.com>
2025-06-30 15:07:00 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Bokep Skandal Artis';
-export const desk = 'Video bokep indo jepang viral ngentot colmek bocil jilbab smp tante.';
+export const desk = 'Video Bokep Indo Jepang Jav Hd Artis Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
 export const url = 'https://bokepskandalartis.pages.dev';
-export const terbit = '2025-06-26T00:00:00Z';
+export const terbit = '2025-06-26T00:01:03Z';
